fb70b261e491e48e8c75c79c769f618f6d6d265b
tranthangbs <84280159+tranthangbs@users.noreply.github.com>
2025-07-16 10:13:06 +0700

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -3,7 +3,7 @@
 # Cấu hình Google Sheet ID cho từng trang/chức năng
 # Mỗi key (ví dụ: 'tasks', 'reports') sẽ được dùng để gọi đúng sheet
 SHEET_IDS = {
-    "tasks": "1rT5EfAGYArPD6pPcdLwx0zRyemiFXZqdAAuBU_-C_1g",
+    "tasks": "1KQ8a_iHRm5RnD8Faz98V-IiA8Qr7pRdeDnI64-2XwYA",
     "reports": "1rT5EfAGYArPD6pPcdLwx0zRyemiFXZqdAAuBU_-C_1g",
     # Thêm các key và ID cho các trang khác ở đây
 }
