fe6b43e20a7eff1e341eda6a340f2d993fc9bc4a
tranthangbs <84280159+tranthangbs@users.noreply.github.com>
2025-07-16 21:52:48 +0700

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
-from .config import SHEET_IDS
+from .config import SHEET_IDS, TASK_SHEET_NAME
 
 # Từ google_sheet_utils.py
 from .google_sheet_utils import get_data_from_sheet, get_sheet_headers, add_row_from_dict
@@ -7,7 +7,7 @@ from .google_sheet_utils import get_data_from_sheet, get_sheet_headers, add_row_
 from .auth_utils import is_authorized, require_role
 
 # Từ view_utils.py
-from .view_utils import set_active_view, render_task_card
+from .view_utils import render_task_card, set_active_view
 
 # Từ data_utils.py
-from .data_utils import search_dataframe, process_deadline_tasks, get_overdue_tasks, filter_latest_tasks_by_name, get_current_hcm_time_str
+from .data_utils import search_dataframe, process_deadline_tasks, get_overdue_tasks, filter_latest_tasks_by_name, get_current_hcm_time_str, get_id_from_url
